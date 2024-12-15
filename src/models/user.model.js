3376c0d7a9d1a35016c31e66e1bec987a542d235
yash kumar jangir <yashjangirme@gmail.com>
2024-12-15 22:24:42 +0530

    updation of jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
-import mongoose  from "mongoose";
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
     {
@@ -38,5 +40,42 @@ const userSchema = new mongoose.Schema(
             type:String
         }
     },{timestamps:true})
+
+    userSchema.pre("save" , async function(next){
+        if(!this.isModified("password")) return next();
+        this.password = await bcrypt.hash(this.password , 10);  
+        next();
+    })
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User" , userSchema)
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,30 +1,8 @@
-const asyncHandler=(requestHandler)=>{
-    (req ,res , next) =>{
-        Promise.resolve(requestHandler(req,res ,next))
-        .catch((err)=>next(err))
-    }
-}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+      .catch((err) => next(err));
+  };
+};
 
-
-
-export {asyncHandler}
-
-
-
-
-
-
-
-
-// this is bascially for try catch we can do it with promise format
-
-// const asyncHandler=(fn)=> async(req , res ,next)=>{
-//     try{
-//         await fn(req ,res ,next)
-//     }catch(error){
-//         res.status(err.code || 500).json({
-//             success:false,
-//             messsage:err.messsage
-//         }) 
-//     }
-// }
+export { asyncHandler };
